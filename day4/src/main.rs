c7998e5106482f77761140b8fe453ff175ec9c96
Tom Meumann <tmeumann@proton.me>
2023-12-05 20:30:30 +1100

    day 4 part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,28 +1,26 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::cmp::min;
+use std::fs::read_to_string;
+use std::io::BufRead;
 
+#[derive(Eq, PartialEq, Hash)]
 struct Game {
     numbers: Vec<u64>,
     winning: Vec<u64>,
 }
 
-fn calculate_score(game: Game) -> u64 {
+fn calculate_score(game: &Game) -> u64 {
     let mut total = 0;
 
     for number in &game.numbers {
         if game.winning.contains(number) {
-            if total == 0 {
-                total = 1;
-            } else {
-                total *= 2;
-            }
+            total += 1;
         }
     }
 
     total
 }
 
-fn read_game(input: String) -> Game {
+fn read_game(input: &str) -> Game {
     let after_colon = input.split(':').last().unwrap();
     let sub_strings: Vec<&str> = after_colon.split('|').map(|s| s.trim()).collect();
 
@@ -44,15 +42,24 @@ fn read_game(input: String) -> Game {
 }
 
 fn main() {
-    let file = File::open("input.txt").expect("Failed to open input file.");
-    let reader = BufReader::new(file);
+    let input = read_to_string("input.txt").expect("Failed to open input file.");
 
-    let total: u64 = reader
-        .lines()
-        .map_while(|l| l.ok())
-        .map(read_game)
-        .map(calculate_score)
-        .sum();
+    let games: Vec<Game> = input.lines().map(read_game).collect();
+    let num_matching_records: Vec<u64> = games.iter().map(calculate_score).collect();
+    let mut card_counts: Vec<u64> = games.iter().map(|_| 1).collect();
 
-    println!("{}", total);
+    for i in 0..num_matching_records.len() {
+        let num_of_this_card = card_counts[i];
+        let num_matching = num_matching_records[i];
+
+        for j in
+            min(card_counts.len(), i + 1)..min(card_counts.len(), 1 + i + num_matching as usize)
+        {
+            card_counts[j] += num_of_this_card;
+        }
+    }
+
+    let total_cards: u64 = card_counts.iter().sum();
+
+    println!("{}", total_cards);
 }
