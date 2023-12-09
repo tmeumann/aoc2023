6952e9af12f1103a761fa543afff7f03dd96a8cc
Tom Meumann <tmeumann@proton.me>
2023-12-09 23:19:14 +1100

    day 9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -15,13 +15,22 @@ fn calculate_next_value(numbers: Vec<i64>) -> i64 {
         + numbers.last().expect("Empty numbers")
 }
 
+fn calculate_previous_value(numbers: Vec<i64>) -> i64 {
+    if numbers.iter().all(|&n| n == 0) {
+        return 0;
+    }
+
+    numbers.first().expect("Empty numbers")
+        - calculate_previous_value(numbers.windows(2).map(|w| w[1] - w[0]).collect())
+}
+
 fn main() {
     let input = read_to_string("input.txt").expect("Failed to read input file");
 
     let result: i64 = input
         .lines()
         .map(parse_line)
-        .map(calculate_next_value)
+        .map(calculate_previous_value)
         .sum();
 
     println!("{result}");
