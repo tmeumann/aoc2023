62ead5338401727f237c916c4164d0081d5cb9ce
Tom Meumann <tmeumann@proton.me>
2023-12-09 23:12:14 +1100

    day 9 part 1

---
+++ day9/src/main.rs
@@ -0,0 +1,28 @@
+use std::fs::read_to_string;
+
+fn parse_line(line: &str) -> Vec<i64> {
+    line.split_whitespace()
+        .flat_map(|s| s.parse::<i64>())
+        .collect()
+}
+
+fn calculate_next_value(numbers: Vec<i64>) -> i64 {
+    if numbers.iter().all(|&n| n == 0) {
+        return 0;
+    }
+
+    calculate_next_value(numbers.windows(2).map(|w| w[1] - w[0]).collect())
+        + numbers.last().expect("Empty numbers")
+}
+
+fn main() {
+    let input = read_to_string("input.txt").expect("Failed to read input file");
+
+    let result: i64 = input
+        .lines()
+        .map(parse_line)
+        .map(calculate_next_value)
+        .sum();
+
+    println!("{result}");
+}
